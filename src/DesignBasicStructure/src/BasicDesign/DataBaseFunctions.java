bbe74a772afaac3d89b69e2e115623261392d445
Ali zoghi <azouqi@gmail.com>
2016-05-16 22:45:01 -0400

    Updated DataBase and Basic Design is added

---
+++ src/DesignBasicStructure/src/BasicDesign/DataBaseFunctions.java
@@ -0,0 +1,10 @@
+package BasicDesign;
+
+import java.sql.ResultSet;
+
+public interface DataBaseFunctions {
+
+	public boolean Create();
+	public boolean update();
+	public boolean delete();
+}
--- src/main/java/com/sepgroup/sep/Main.java
+++ src/main/java/com/sepgroup/sep/Main.java
@@ -1,5 +1,6 @@
 package com.sepgroup.sep;
 
+import java.sql.ResultSet;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,9 +16,15 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
-
+    
+    
     public static void main(String[] args) {
         launch(args);
+		// TODO Auto-generated method stub
+		Database db= new Database();
+		project p= new project("project test", "2015-05-23", "2015-06-25", 2500, false);
+		System.out.println(p.toString());
+		String sql= "SELECT * FROM Project;";
+		System.out.println(project.findBySql(sql));
     }
 }
