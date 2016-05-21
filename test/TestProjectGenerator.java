cf6d9751980d67e69709d928f5c27704d2e1f181
Wishe Deom <wishedeom@gmail.com>
2016-05-21 19:14:11 -0400

    Implemented getRandomName method.

--- test/TestProjectGenerator.java
+++ test/TestProjectGenerator.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Created by Wishe on 5/21/2016.
  */
@@ -5,5 +7,10 @@
 // Can generate many sorts of random projects for testing the application
 public class TestProjectGenerator
 {
+	private static Random random = new Random();
 
+	public static String getRandomName()
+	{
+		return null;
+	}
 }
