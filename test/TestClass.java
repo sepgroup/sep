a0b26b7857382078ee5010e28ad99bf8465302b6
Demo <Demo.kioussis@gmail.com>
2016-05-29 20:10:20 -0400

    Created test class

---
+++ test/TestClass.java
@@ -0,0 +1,14 @@
+/**
+ * Created by HP on 5/29/2016.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestClass {
+    @Test
+    public void testTest(){
+        assertEquals(5,5,0.005);
+    }
+
+}
