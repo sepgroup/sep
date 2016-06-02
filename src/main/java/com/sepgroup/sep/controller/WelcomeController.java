5532e54ff5788334598df1dfb8e174c7b9157b7b
Jeremy Brown <jj.brown@hotmail.com>
2016-06-01 22:10:32 -0400

    Implement welcome screen functionality.
    Also fix a potential bug in Main.setPrimaryScene()

--- src/main/java/com/sepgroup/sep/controller/WelcomeController.java
+++ src/main/java/com/sepgroup/sep/controller/WelcomeController.java
@@ -1,19 +1,57 @@
 package com.sepgroup.sep.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.sepgroup.sep.Main;
 
+import com.sepgroup.sep.model.ModelNotFoundException;
+import com.sepgroup.sep.model.ProjectModel;
+import com.sun.javafx.collections.ImmutableObservableList;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 
 /**
  * Created by jeremybrown on 2016-05-17.
  */
 public class WelcomeController extends AbstractController {
 
+    @FXML
+    public ListView<ProjectModel> existingProjectsList;
+
+    @FXML
+    public Button createNewProjectButton;
+
+    @FXML
+    public Button openSelectedProjectButton;
+
+    ProjectModel selectedProject;
+
+
     public WelcomeController() {
         setFxmlPath("/views/welcome.fxml");
         setCssPath("/style/stylesheet.css");
+    }
+
+    @FXML
+    public void initialize(){
+        // Will be called by FXMLLoader
+        populateExistingProjects();
+    }
+
+    private void populateExistingProjects() {
+        try {
+            List<ProjectModel> projects = ProjectModel.getAll();
+            ObservableList<ProjectModel> observableProjectsList = new ImmutableObservableList<>();
+            observableProjectsList.addAll(projects);
+            existingProjectsList.setItems(observableProjectsList);
+        }
+        catch (ModelNotFoundException e) {
+            existingProjectsList.setDisable(true);
+        }
+
     }
 
     /**
@@ -25,18 +63,27 @@ public class WelcomeController extends AbstractController {
             Main.setPrimaryScene(new ProjectCreatorController());
         } catch (IOException e) {
             e.printStackTrace();
+            // TODO popup or something
         }
+    }
+
+    @FXML
+    public void onProjectListSelected() {
+        selectedProject = existingProjectsList.getSelectionModel().getSelectedItem();
+        if (selectedProject != null) openSelectedProjectButton.setDisable(false);
     }
 
     /**
      * Moves to editview 
      */
     @FXML
-    public void onEditProjectClicked() {
+    public void onOpenSelectedProjectClicked() {
+        ProjectModel selectedProject = existingProjectsList.getSelectionModel().getSelectedItem();
         try {
-            Main.setPrimaryScene(new ProjectEditorController());
+            Main.setPrimaryScene(new ProjectViewerController(selectedProject));
         } catch (IOException e) {
             e.printStackTrace();
+            // TODO popup or something
         }
     }
 
