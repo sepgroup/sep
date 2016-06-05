489faf65c0922fc94aebc29d88e6e991e14238b6
Demo <Demo.kioussis@gmail.com>
2016-06-04 23:11:58 -0400

    Testing updates

--- src/test/java/TestingTools/ProjectTest.java
+++ src/test/java/TestingTools/ProjectTest.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import static org.junit.Assert.*;
 
 /**
- * Created by HP on 5/29/2016.
+ * Created by Demo on 5/29/2016.
  */
 
 public class ProjectTest {
@@ -24,58 +24,66 @@ public class ProjectTest {
     public void projectGenerationTest() {
         int numOfTests = 10;
         int passCount = 0;
+        ProjectModel[] projectsIn = new ProjectModel[numOfTests];
+        ProjectModel[] projectsOut = new ProjectModel[numOfTests];
+
         System.out.println("\n\nTESTING PROJECT GENERATION\n");
         for (int i = 0; i < numOfTests; i++) {
-            Pair<Date>  dates = null;
+            Pair<Date> dates = null;
             try {
-                dates =RandomDateBuilder.randomDatePair(true);
-            }
-            catch(Exception e){
+                dates = RandomDateBuilder.randomDatePair(true);
+            } catch (Exception e) {
                 System.out.println("GENERAL ERROR ");
             }
 
-            String name = RandomStringBuilder.randomString((int)(Math.random()*100+1));
+            String name = "Chinchi";// RandomStringBuilder.randomString((int)(Math.random()*100+1));
             Date ds = dates.first;
             Date de = dates.second;
 
-            double budget = Math.random()*1000000;
-            boolean done = Math.random()<0.5;
+            double budget = Math.random() * 1000000;
+            boolean done = Math.random() < 0.5;
             int id = i;//(int)(Math.random()*1000);
 
-            String description = RandomStringBuilder.randomString((int)(Math.random()*1000+1));
+            String description = "Kind of chinchilla";//RandomStringBuilder.randomString((int)(Math.random()*1000+1));
 
 
-            testProject = new ProjectModel(name, ds, de, budget, done, id, description);
+            projectsIn[i] = new ProjectModel(name, ds, de, budget, done, id, description);
             try {
-                testProject.persistData();
-            }
-            catch (DBException e) {
+                projectsIn[i].persistData();
+            } catch (DBException e) {
                 System.out.println(e.getLocalizedMessage());
                 break;
             }
+        }
+        for (int i = 0; i < numOfTests; i++) {
+            System.out.println("TEST ITERATION : " + i);
+            System.out.println("Name: " + projectsIn[i].getName());
+            System.out.println("Date start: " +  projectsIn[i].getStartDate());
+            System.out.println("Date end: " +  projectsIn[i].getDeadline());
+            System.out.println("Budget: " + projectsIn[i].getBudget());
+            System.out.println("Done: " + projectsIn[i].isDone());
+            System.out.println("ID: " + projectsIn[i].getProjectId());
+            System.out.println("Description: " + projectsIn[i].getProjectDescription() + "\n");
+
+            try{
+                projectsOut[i] = ProjectModel.getById(i);
 
 
-            System.out.println("TEST ITERATION : "+i );
-            System.out.println("Name: "+name );
-            System.out.println("Date start: "+dates.first);
-            System.out.println("Date end: "+dates.second);
-            System.out.println("Budget: "+budget);
-            System.out.println("Done: "+done);
-            System.out.println("ID: "+id);
-            System.out.println("Description: "+description+"\n");
+            }catch(Exception e){
+
+                System.out.println("COULD NOT PULL FROM DATABASE");
+            }
 
 
-
-            assertEquals("Name test", name, testProject.getName());
-            assertEquals("Date start test", dates.first, testProject.getStartDate());
-            assertEquals("Date end test", dates.second, testProject.getDeadline());
-         //   assertEquals()
-            assertEquals("Budget test", budget, testProject.getBudget(), 0.00001);
-            assertEquals("Done test", done, testProject.isDone());
-            assertEquals("ID test", 0, testProject.getProjectId());
-            assertEquals("Description test", description, testProject.getProjectDescription());
+            assertEquals("Name test", projectsIn[i].getName(),projectsOut[i].getName());
+            assertEquals("Date start test", projectsIn[i].getStartDate(),projectsOut[i].getStartDate());
+            assertEquals("Date end test", projectsIn[i].getDeadline(),projectsOut[i].getDeadline());
+            assertEquals("Budget test", projectsIn[i].getBudget(),projectsOut[i].getBudget(), 0.00001);
+            assertEquals("Done test", projectsIn[i].isDone(),projectsOut[i].isDone());
+            assertEquals("ID test", projectsIn[i].getProjectId(),projectsOut[i].getProjectId());
             passCount++;
         }
+
         System.out.println("PROJECT GENERATION PASSED "+passCount+"/"+numOfTests+" TESTS");
     }
 
