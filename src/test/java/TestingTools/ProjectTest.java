5417fc42bac238d76b227236ba977cf56be6b611
Demo <Demo.kioussis@gmail.com>
2016-06-04 20:59:01 -0400

    Updating testing

--- src/test/java/TestingTools/ProjectTest.java
+++ src/test/java/TestingTools/ProjectTest.java
@@ -1,5 +1,7 @@
 package TestingTools;
 
+import com.sepgroup.sep.controller.DialogCreator;
+import com.sepgroup.sep.db.DBException;
 import com.sepgroup.sep.model.ModelNotFoundException;
 import com.sepgroup.sep.model.ProjectModel;
 import com.sepgroup.sep.model.TaskModel;
@@ -20,29 +22,39 @@ public class ProjectTest {
     static ProjectModel testProject;
     @Test
     public void projectGenerationTest() {
-        int numOfTests = 1000;
+        int numOfTests = 10;
+        int passCount = 0;
         System.out.println("\n\nTESTING PROJECT GENERATION\n");
         for (int i = 0; i < numOfTests; i++) {
             Pair<Date>  dates = null;
             try {
-                dates =RandomDateBuilder.randomDatePair(false);
+                dates =RandomDateBuilder.randomDatePair(true);
             }
             catch(Exception e){
                 System.out.println("GENERAL ERROR ");
             }
 
-            String name = RandomStringBuilder.randomString((int)Math.random()*100+1);
+            String name = RandomStringBuilder.randomString((int)(Math.random()*100+1));
             Date ds = dates.first;
             Date de = dates.second;
 
             double budget = Math.random()*1000000;
             boolean done = Math.random()<0.5;
-            int id = (int)Math.random()*1000;
+            int id = i;//(int)(Math.random()*1000);
 
-            String description = RandomStringBuilder.randomString((int)Math.random()*1000+1);
+            String description = RandomStringBuilder.randomString((int)(Math.random()*1000+1));
 
 
             testProject = new ProjectModel(name, ds, de, budget, done, id, description);
+            try {
+                testProject.persistData();
+            }
+            catch (DBException e) {
+                System.out.println(e.getLocalizedMessage());
+                break;
+            }
+
+
             System.out.println("TEST ITERATION : "+i );
             System.out.println("Name: "+name );
             System.out.println("Date start: "+dates.first);
@@ -54,18 +66,17 @@ public class ProjectTest {
 
 
 
-
-
-
             assertEquals("Name test", name, testProject.getName());
             assertEquals("Date start test", dates.first, testProject.getStartDate());
             assertEquals("Date end test", dates.second, testProject.getDeadline());
+         //   assertEquals()
             assertEquals("Budget test", budget, testProject.getBudget(), 0.00001);
             assertEquals("Done test", done, testProject.isDone());
-            assertEquals("ID test", id, testProject.getProjectId());
+            assertEquals("ID test", 0, testProject.getProjectId());
             assertEquals("Description test", description, testProject.getProjectDescription());
+            passCount++;
         }
-        System.out.println("PROJECT GENERATION PASSED "+numOfTests+" TESTS");
+        System.out.println("PROJECT GENERATION PASSED "+passCount+"/"+numOfTests+" TESTS");
     }
 
     @Test
