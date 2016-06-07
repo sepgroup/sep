8aab4b9676bf20a18976ff4447f77bc4cf5979bf
Npolesana <nicolapolesana@gmail.com>
2016-06-07 11:41:57 -0400

    Style added in project viewer

--- src/main/java/com/sepgroup/sep/Main.java
+++ src/main/java/com/sepgroup/sep/Main.java
@@ -33,8 +33,8 @@ public class Main extends Application {
         primaryStage.setMaxHeight(800);
         primaryStage.setMaxWidth(1000);
         
-        primaryStage.setMinHeight(500);
-        primaryStage.setMinWidth(900);
+        primaryStage.setMinHeight(560);
+        primaryStage.setMinWidth(810);
         
 
         primaryStage.show();
