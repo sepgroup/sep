406bd15eb0f8c62d3036112cc5c2d51b49144afc
Demo <Demo.kioussis@gmail.com>
2016-08-03 19:35:31 -0400

    Worked on Factory pattern

--- src/main/java/com/sepgroup/sep/analysis/GraphFactory.java
+++ src/main/java/com/sepgroup/sep/analysis/GraphFactory.java
@@ -1,6 +1,11 @@
 package com.sepgroup.sep.analysis;
 
 
+import com.sepgroup.sep.model.ModelNotFoundException;
+import com.sepgroup.sep.model.TaskModel;
+
+import java.util.List;
+
 /**
  * Created by Demo on 7/29/2016.
  */
@@ -23,11 +28,32 @@ public class GraphFactory {
 
     // pull each task related to the project and assign it to a node
     private static void pullTasks(int projectID){
-
+        List<TaskModel> tasks = null;
+        try {
+             tasks = TaskModel.getAllByProject(projectID);
+        }
+        catch(ModelNotFoundException E){
+            System.out.println("Project"+ projectID +" Not Found");
+            return;
+        }
+        for(TaskModel t : tasks){
+            Data d = new Data(t);
+            Node n = new Node(d);
+            n.setNodeID(t.getTaskId());
+            graph.addNode(n);
+        }
+        //graph.sort();
     }
 
     // set adjacencies between each task using nodes
     private static void setAdjacents(int projectID){
+        List<TaskModel> dependencies = null;
+            for(Node n : graph.nodes){
+                dependencies = n.getData().task.getDependencies();
+                for(TaskModel t:dependencies){
+                    graph.addDirectedEdge(graph.getNodeByID(t.getTaskId()),n);
+                }
+            }
 
     }
 }
